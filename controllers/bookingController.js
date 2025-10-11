260f195c42279c0601a0fb5e3f9fc689f95ae4d8
Mustafa Khaled <mostafakhaled0787314@gmail.com>
2025-10-11 11:52:47 +0300

    create booking checkout controller

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,5 +1,6 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('../models/tourModel');
+const Booking = require('../models/bookingModel');
 const AppError = require('../utils/appError');
 const { catchAsync } = require('../utils/catchAsync');
 
@@ -13,7 +14,7 @@ const getCheckoutSession = catchAsync(async (req, res, next) => {
   // 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/`,
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${tourId}&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: tourId,
@@ -41,4 +42,15 @@ const getCheckoutSession = catchAsync(async (req, res, next) => {
   });
 });
 
-module.exports = { getCheckoutSession };
+const createBookingCheckout = catchAsync(async (req, res, next) => {
+  // This is only TEMPORARY, because it's UNSECURE: everyone can make bookings without paying
+  const { tour, user, price } = req.query;
+
+  if (!tour || !user || !price) return next();
+
+  await Booking.create({ tour, user, price });
+
+  res.redirect(req.originalUrl.split('?')[0]);
+});
+
+module.exports = { getCheckoutSession, createBookingCheckout };
